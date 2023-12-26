042bc29d0e1eca6b00564d883c29742d311acfc0
Iris Michelsen <sarah.iris.michelsen@gmail.com>
2023-12-26 14:00:25 -0800

    trying to commit

---
+++ PyBank/main.py
@@ -0,0 +1,17 @@
+#Count num of rows-first row (to determine num of months)
+
+#Read numbers in right column
+#Cast numbers as integers
+#Add all the numbers together
+
+#set variables to 0
+#Loop through all rows and get changes and add together 
+#(TotalChange = TotalChange + CurrentChange)
+#Divide the TotalChange by # of months (or months - 1)
+
+#in the same loop
+#if CurrentChange is > GreatInc, GreatInc = CurrentChange
+#if CurrentChange < GreatDec, GreatDec = CurrentChange
+
+#print analysis to terminal
+#export to a file
---
+++ PyPoll/main.py
@@ -0,0 +1,11 @@
+#Count number of rows-first row
+
+#Loop through file
+#Create dictionary containing candidates names + vote counts
+
+#Divide each candidates vote count by total vote count
+
+#Determine winner by who has the most votes
+
+#Print results to terminal
+#Export a text file with results
